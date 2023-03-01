9d4e34dab27eb6096b1edce743d373bbf88518bf
EC2 Default User <ec2-user@ip-172-31-43-171.ap-northeast-1.compute.internal>
2023-03-01 15:31:26 +0000

    「Update」20230301

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
   
+  namespace :admin do
+    
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:index, :show]
+    
+  end
   # 顧客用
  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
